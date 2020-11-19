6ccf44484689738ab78257d8ea29ac713104b9ee
hiep <hiep@gmail.com>
2020-11-19 18:27:38 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import {
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 import HatsPage from './pages/hatspage/hatspage.component';
 
 function App() {
   return (
     <Router>
       <div>
+        <Header/>
         <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage}/>
