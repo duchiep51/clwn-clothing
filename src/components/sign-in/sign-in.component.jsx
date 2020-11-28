0fcf55f47dd784c1e9168f5d23642db68e437916
hiep <hiep@gmail.com>
2020-11-28 20:44:33 +0700

    finish sign in and sign out

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
@@ -16,13 +16,17 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        // this.state = {
-        //     email: '',
-        //     password: ''
-        // }
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+
+            this.setState({email: '', password: ''});
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     handleChange = event => {
@@ -41,6 +45,7 @@ class SignIn extends React.Component {
                     <FormInput name='password' type='password' label='password' handleChange={this.handleChange} value={this.state.password} required/>
 
                     <div className='buttons'>
+                        <CustomButton type='submit'>Sign in</CustomButton>
                         <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
                     </div>
                 </form>
